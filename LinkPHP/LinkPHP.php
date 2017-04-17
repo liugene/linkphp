72dcb9bff1393eefe788489b6e04aa27c053e0ab
LinkPHP <750688237@qq.com>
2017-04-17 21:12:44 +0800

    接上一版本更新

--- LinkPHP/LinkPHP.php
+++ LinkPHP/LinkPHP.php
@@ -35,7 +35,7 @@
  //定义LinkPHP框架公共配置目录常量
  define('CONF_PATH', LINKPHP_PATH . 'Conf/');
  //定义LinkPHP框架应用公共配置目录常量
- define('APPCONF_PATH', ROOT_PATH . 'Config/');
+ define('APPCONF_PATH', ROOT_PATH . 'Inc/Config/');
  //定义LinkPHP框架扩展类库目录常量
  define('EXTEND_PATH', LINKPHP_PATH . 'Extend/');
  //定义LinkPHP框架附件目录常量
