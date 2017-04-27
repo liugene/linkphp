0049addb103c6a6ddd02a1348dbad5bc2b5d32c4
company <jun.liu@jrexpo.com>
2017-04-27 14:23:07 +0800

    路由自定义配置支持

--- Inc/route.inc.php
+++ Inc/route.inc.php
@@ -9,7 +9,10 @@ return array(
     'DEFAULT_CONTROLLER' => 'Index',  //默认控制器
     'DEFAULT_ACTION'     => 'index',  //默认操作方法
     'URL_MODULE'         => '1',      //URL模式 0普通模式 1 pathinfo模式 2 rewrite模式
-    'ROUTE_RULES'        => array(),
+    'ROUTE_RULES_ON'     => 'TRUE',
+    'ROUTE_RULES'        => array(
+        '/Index/Index'   =>  '/Index/Api',
+    ),
 );
 
 ?>
