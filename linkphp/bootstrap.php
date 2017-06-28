67ad3eb9f6a0a2be053cfc4a22bafd637184a13a
company <jun.liu@jrexpo.com>
2017-06-28 11:57:24 +0800

    接上一版本

---
+++ linkphp/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * --------------------------------------------------*
+ *  LhinkPHP遵循Apache2开源协议发布  Link ALL Thing  *
+ * --------------------------------------------------*
+ *  @author LiuJun     Mail-To:liujun2199@vip.qq.com *
+ * --------------------------------------------------*
+ * Copyright (c) 2017 LinkPHP. All rights reserved.  *
+ * --------------------------------------------------*
+ *                 LinkPHP框架启动文件               *
+ * --------------------------------------------------*
+ */
+
+ namespace link;
+
+ /**
+  * 加载Composer自动加载
+  */
+ require VENDOR_PATH . 'autoload.php';
+
+ /**
+  * 加载LinkPHP框架核心初始化类
+  */
+ require CORE_PATH . 'Console' . EXT;
+
+ Console::start();
