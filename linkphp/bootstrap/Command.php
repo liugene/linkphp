6eaff4349fe705e06375d00b49f855b08461fa08
company <jun.liu@jrexpo.com>
2017-07-20 08:39:49 +0800

    增加命令行提示

--- linkphp/bootstrap/Command.php
+++ linkphp/bootstrap/Command.php
@@ -18,8 +18,41 @@ class Command
     static public function init()
     {
         if(isset($_SERVER['SHELL'])){
-            print_r($_SERVER['argv']);
-            print_r($_SERVER['argc']);
+            if($_SERVER['argc'] == 1){
+                echo <<<EOT
+Link Console version 0.1
+
+Usage:
+  command [options] [arguments]
+
+Options:
+  -h, --help            Display this help message
+  -V, --version         Display this console version
+  -q, --quiet           Do not output any message
+  --ansi                Force ANSI output
+  --no-ansi             Disable ANSI output
+  -n, --no-interaction  Do not ask any interactive question
+  -v|vv|vvv, --verbose  Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug
+
+Available commands:
+  build              Build Application Dirs
+  clear              Clear runtime file
+  help               Displays help for a command
+  list               Lists commands
+ make
+  make:controller    Create a new resource controller class
+  make:model         Create a new model class
+ optimize
+  optimize:autoload  Optimizes PSR0 and PSR4 packages to be loaded with classmaps too, good for production.
+  optimize:config    Build config and common file cache.
+  optimize:route     Build route cache.
+EOT;
+            } else {
+                echo <<<EOT
+  method not defined
+EOT;
+
+            }
             exit;
         }
     }
