c3c66fe8cea98bf247f478d8a2b6116989c9f526
LinkPHP <750688237@qq.com>
2017-09-16 16:37:05 +0800

    优化linux系统下路径错误

--- loaders/route.php
+++ loaders/route.php
@@ -7,7 +7,7 @@ return [
     'var_controller'     => 'c',      //默认传递控制器名
     'var_action'         => 'a',      //默认传递方法名
     'default_platform'   => 'main',  //默认模块名
-    'default_controller' => 'home',  //默认控制器
+    'default_controller' => 'Home',  //默认控制器
     'default_action'     => 'main',  //默认操作方法
     'url_module'         => '1',      //URL模式 0普通模式 1 pathinfo模式 2 rewrite模式
     'route_rules_on'     => TRUE,
