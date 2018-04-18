58b2ac105f5865753f6d76490073fed3cdf90f30
liugene <750688237@qq.com>
2018-04-18 14:58:57 +0800

    优化入口方法

--- website/index.php
+++ website/index.php
@@ -15,11 +15,11 @@
 //目录基础常量的定义
 define('ROOT_PATH', dirname(__DIR__) . '/');
 //加载LinkPHP框架常量文件
-require_once(ROOT_PATH . '/vendor/linkphp/framework/src/define.php');
+require(ROOT_PATH . 'vendor/linkphp/framework/src/define.php');
 //加载自动加载方法
-require_once(VENDOR_PATH . 'linkphp/loader/src/Loader.php');
+require(VENDOR_PATH . 'linkphp/loader/src/Loader.php');
 //加载LinkPHP框架启动文件
-require_once(ROOT_PATH . '/bootstrap.php');
+require(ROOT_PATH . 'bootstrap.php');
 
 //只需要这么几句话就可以运行 !><!
 //是不是很轻便呀 喵~
