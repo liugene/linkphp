dba528fc4911e1e3e8cc72227528528f80f6941d
liugene <750688237@qq.com>
2018-05-22 21:47:50 +0800

    更改config配置目录名

--- bin/command/WebSocket.php
+++ bin/command/WebSocket.php
@@ -2,14 +2,24 @@
 
 namespace bin\command;
 
+use linkphp\process\Process;
 use swoole_websocket_server;
 use linkphp\Application;
 use linkphp\console\Command;
+use linkphp\process\drives\Swoole;
 
 class WebSocket extends Command
 {
 
+    /**
+     * @var swoole_websocket_server
+     */
     private $webscoket;
+
+    /**
+     * @var Process
+     */
+    private $process;
 
     public function configure()
     {
@@ -18,27 +28,48 @@ class WebSocket extends Command
 
     public function execute()
     {
-        $server = new swoole_websocket_server("127.0.0.1", 9502);
-
-        $server->on('open', function($server, $req) {
-            Application::get('linkphp\console\command\Output')->writeln("connection open: {$req->fd}\n");
-        });
-
-        $server->on('message', function($server, $frame) {
-            Application::get('linkphp\console\command\Output')->writeln("received message: {$frame->data}\n");
-            $server->push($frame->fd, $frame->data);
-        });
-
-        $server->on('close', function($server, $fd) {
-            Application::get('linkphp\console\command\Output')->writeln("connection close: {$server}\n");
-        });
-        $this->webscoket = $server;
-        $server->start();
+//        $server = new swoole_websocket_server("127.0.0.1", 9502);
+//
+//        $server->on('open', function($server, $req) {
+//            Application::get('linkphp\console\command\Output')->writeln("connection open: {$req->fd}\n");
+//        });
+//
+//        $server->on('message', function($server, $frame) {
+//            Application::get('linkphp\console\command\Output')->writeln("received message: {$frame->data}\n");
+//            $server->push($frame->fd, $frame->data);
+//        });
+//
+//        $server->on('close', function($server, $fd) {
+//            Application::get('linkphp\console\command\Output')->writeln("connection close: {$server}\n");
+//        });
+//        $this->webscoket = $server;
+//        $server->start();
     }
 
     //操作命名
-    public function commandHandle($command) {
-        dump($this->webscoket);die;
+    public function commandHandle($command, $process) {
+        if(!isset($this->process)){
+            $this->process = $process;
+        }
+        if($command == 'start'){
+            $server = new swoole_websocket_server("127.0.0.1", 9502);
+
+            $server->on('open', function($server, $req) {
+                Application::get('linkphp\console\command\Output')->writeln("connection open: {$req->fd}\n");
+            });
+
+            $server->on('message', function($server, $frame) {
+                Application::get('linkphp\console\command\Output')->writeln("received message: {$frame->data}\n");
+                $server->push($frame->fd, $frame->data);
+            });
+
+            $server->on('close', function($server, $fd) {
+                Application::get('linkphp\console\command\Output')->writeln("connection close: {$server}\n");
+            });
+            $this->webscoket = $server;
+            $server->start();
+            return;
+        }
         if ($command == 'status') {
             $stats = $this->webscoket->stats();
             Application::get('linkphp\console\command\Output')->writeln("查看状态: {$stats}\n");
@@ -50,7 +81,8 @@ class WebSocket extends Command
             return;
         }
         if ($command == 'stop') {
-            $this->webscoket->stop() && $this->webscoket->shutdown();
+            dump($this->process->stop());die;
+            $this->webscoket->stop() && $this->webscoket->shutdown() && $this->process->stop();
             Application::get('linkphp\console\command\Output')->writeln('成功停止');
             return;
         }
---
+++ config/httpd.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    // HttpServer
+    'httpServer' => [
+        // 类路径
+        'class'        => 'linkphp\swoole\HttpServer',
+        // 主机
+        'host'         => '127.0.0.1',
+        // 端口
+        'port'         => 9508,
+        // 运行时的各项参数：https://wiki.swoole.com/wiki/page/274.html
+        'setting'      => [
+            // 连接处理线程数
+            'reactor_num' => 8,
+            // 工作进程数
+            'worker_num'  => 8,
+            // 设置 worker 进程的最大任务数
+            'max_request' => 10000,
+            // PID 文件
+            'pid_file'    => '/var/run/link-httpd.pid',
+            // 日志文件路径
+            'log_file'    => '/tmp/link-httpd.log',
+            // 子进程运行用户
+            /* 'user'        => 'www', */
+        ],
+    ]
+];
